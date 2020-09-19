0cd27f3734ded391ccfef9619a5d68dbc5388858
rifatdlynx <rifatlynx@gmail.com>
2020-09-19 15:27:14 +0600

    2nd Push
    Images folder merged
    Added footer

--- includes/side_nav.php
+++ includes/side_nav.php
@@ -3,16 +3,16 @@
         <table style="background-color: #1f1f1f;">
             <tr>
                 <td style=" background-color: #1f1f1f;"><a href="#" class="active" style="padding-right:30px;">Liked Songs</a></td>
-                <td style=" background-color: #1f1f1f;"><button class="editable" style="margin-top: 5px"><img src="assets/images/liked_songs.png"></button></td>
+                <td style=" background-color: #1f1f1f;"><button class="editable" style="margin-top: 5px"><img src="assets/images/icon/liked_songs.png"></button></td>
             </tr>
             <tr>
                 <td style=" background-color: #1f1f1f; border-bottom:1px solid #fff;"><a href="#" style="margin-bottom: 5px">History</a></td>
-                <td style=" background-color: #1f1f1f; border-bottom:1px solid #fff;"><button class="editable" style="margin-bottom: 5px; padding-right:20px;"><img src="assets/images/history.png"></button></td>
+                <td style=" background-color: #1f1f1f; border-bottom:1px solid #fff;"><button class="editable" style="margin-bottom: 5px; padding-right:20px;"><img src="assets/images/icon/history.png"></button></td>
             </tr>
             <tr>
             <tr>
                 <td style="padding-right:0px; background-color: #1f1f1f;"  ><a href="#"  style="margin-top: 5px">Create Playlist</a></td>
-                <td style=" background-color: #1f1f1f;"><button class="editable" style="margin-top: 5px"><img src="assets/images/create_playlist.png"></button></td>
+                <td style=" background-color: #1f1f1f;"><button class="editable" style="margin-top: 5px"><img src="assets/images/icon/create_playlist.png"></button></td>
             </tr>
             
             <?php
@@ -24,7 +24,7 @@
                         <td style= \"padding-right:0px; padding-bottom:3px; background-color: #1f1f1f;\">
                         <a href=\"#\">Playlist " , $x , "</a></td>
                     
-                        <td style=\" background-color: #1f1f1f;\"><button class=\"editable\"><img src=\"assets/images/editable15px.png\"></button></td>
+                        <td style=\" background-color: #1f1f1f;\"><button class=\"editable\"><img src=\"assets/images/icon/editable15px.png\"></button></td>
                     
                     </tr>";
                 }
