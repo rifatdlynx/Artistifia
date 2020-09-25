418b504240af933bc37639731403b9b296b05201
Umme-Hani-Era <68460554+Umme-Hani-Era@users.noreply.github.com>
2020-09-25 10:29:20 +0600

    Play And Update

--- artist_song_view.php
+++ artist_song_view.php
@@ -1,11 +1,9 @@
 <?php include('includes/header.php'); ?>
     <?php 
-    $album_id = $_GET['album_id'];
+    $artist_id = $_GET['artist_id'];
     
-    $albumQuery = mysqli_query($con, "SELECT albums.name AS album, artists.name AS artist, artists.id AS bleh, album_art_path FROM albums 
-    INNER JOIN artists ON albums.artist = artists. id WHERE albums.id =".$album_id );
-    $album = mysqli_fetch_array ($albumQuery,  MYSQLI_ASSOC);
-    
+    $artistQuery = mysqli_query($con, "SELECT * FROM artists WHERE artists.id =".$artist_id );
+    $artist = mysqli_fetch_array ($artistQuery,  MYSQLI_ASSOC);
 
     echo"
     <div class=\"entityInfo\"
@@ -13,17 +11,16 @@
 
     <div class=\"leftSection\" 
     style =\"width: 30%; float: left; max-width: 250px;\">
-	<img src=\"assets/" .$album['album_art_path']."\" style = \"width: 100%; \">
+	<img src=\"assets/" .$artist['image_path']."\" style = \"width: 200px; height:200px; object-fit: cover;  border-radius: 50%; \">
 	</div>
 
     <div class=\"rightSection\"
-    style = \"width: 70%; float: left; padding: 5px 10px 5px 40px; box-sizing: border-box;\">
-        <h1 style =\"margin-top: 0px; color: silver;\">". $album['album']. "</h1>    
-        <h2 onclick='location.href=\"artist_song_view.php?artist_id=".$album['bleh']. "\"'
-        style =\"margin-top: 0px; color: silver; cursor: pointer;\">". $album['artist']. "</h2>
-		<p role=\"link\" tabindex=\"0\" style =\"color: #939393; font-weight: 200;\"></p>
+    style = \"width: 70%; float: left; padding: 5px 10px 5px 40px; box-sizing: border-box;\">   
+        <h1 style =\"margin-top: 0px; color: silver\">". $artist['name']. "</h1>
+		<p role=\"link\" tabindex=\"0\" ? style =\"color: #939393; font-weight: 200;\"></p>
         <p style =\"color: #939393; font-weight: 200;\">";
-        $songQuery = mysqli_query($con, "SELECT count(id) AS sum FROM songs WHERE album =". $album_id);
+        $songQuery = mysqli_query($con, "SELECT count(songs.id) AS sum FROM songs INNER JOIN albums ON songs.album = albums.id
+                    WHERE albums.artist =". $artist_id);
         echo mysqli_fetch_array ($songQuery,  MYSQLI_ASSOC)['sum']." songs</p>
 
 	</div>
@@ -31,44 +28,33 @@
 </div>
 
 
-<div class=\"tracklistContainer\" style=\"padding: 0px 0px 5px 30px\" >
+<div class=\"tracklistContainer\" style=\"padding: 0px 5px 0px 30px; margin-bottom: 100px ; \" >
     <ul class=\"tracklist\" 
     style=\" background-color: #2b2b2b\">";
 		
-		$songQuery = mysqli_query($con, "SELECT * FROM songs WHERE album =". $album_id." ORDER BY track_no");
+		$songQuery = mysqli_query($con, "SELECT *, songs.id AS songId FROM songs INNER JOIN albums ON songs.album = albums.id
+         WHERE albums.artist =". $artist_id." ORDER BY streams DESC LIMIT 5");
         
-        echo "<li class='tracklistRow' style=\"padding: 5px 5px 5px 0px; margin-bottom: 5px; border-top: 1px solid #a0a0a0; border-bottom: 1px solid #a0a0a0;\">
-					<div class='trackCount'>
-						<span class='trackNumber' style=\"padding: 0px 0px 0px 63px;\" > # </span>
-                    </div>
-                    
-					<div class='trackInfo'>
-						<span class='trackName'style=\"padding: 0px 0px 0px 10px;\"> Track Name </span>
-						
-					</div>
-					
-					<div class='trackDuration'>
-						<span class='duration'> Duration </span>
-                    </div>
-                    
-                </li>";
+        echo " <h3 style=\"color: white; margin-left: 10px; padding-bottom: 5px; border-bottom: 1px solid #a0a0a0\"> Popular <h3>";
+        
         
          $i=1;
+         $song=array();
         while($row = mysqli_fetch_array($songQuery)) {
-            
+            $song[]= $row;
 			echo "<li  class='tracklistRow'>
                     <div class='trackCount'>
                         <span class='trackNumber'style=\"font-size: 15px;\">
                         <audio id = 'Audio".$i."' src = \"assets/".$row['file_path']."\" ></audio>
-                        <img id ='play".$i."' src='assets/images/icon/play.png' onclick = 'play(".$i.", ".$row['id'].")'>".$row['track_no'].
-                        "</span>
+                        <img id ='play".$i."' src='assets/images/icon/play.png' onclick = 'play(".$i.", ".$row['songId'].")'>".$i."
+                        </span>
                     </div>
                     
                     <div id= 'songs' class='trackInfo' >
                         
                         <span class='trackName' style=\"font-size: 17px; color: #fff;\">" . $row['title'] . "</span>
-                        <span class='trackArtist' style=\"font-size: 14px;\">" . $album['artist'];
-                        if(isset($row['featuring_artist'])) echo ", ".$row['featuring_artist'];
+                        <span id ='streams".$i."' class='trackArtist' style=\"font-size: 14px;\">" . $row['streams'];
+                        
                         echo "</span>
                     </div>
                     
@@ -80,10 +66,12 @@
 				</li>";
                 $i++;
 		}
-        echo "</ul>";
+        echo "</ul></div>";
+        
         ?>
 <script>
     var audio;
+    var plays;
     function play(id, temp) {
         audio = document.getElementById("Audio" + id);
         var button = document.getElementById("play"+ id);
@@ -98,11 +86,19 @@
             button.src = "assets/images/icon/pause.png";
             button.style.opacity = "20%";
         }
-        if(audio.currentTime === 0) {up(temp);};
+        if(audio.currentTime === 0) {up(id, temp);};
     };
-    function up(temp){
-        $.post("includes/updateStreams.php", {songId: temp});
+    function up(id, temp){
+        $.post("includes/updateStreams.php", {songId: temp}, function(data){
+            var newStream = data;
+            plays = document.getElementById("streams" + id);
+            console.log(plays.innerHTML + " newStream = " + newStream);
+            plays.innerHTML = newStream;
+        });
+
     };
+    
+    
 </script>
 <style>
                     .tracklistRow {
@@ -159,10 +155,31 @@
                         text-align: right;
                     }
                     
-
-                
                     </style>
-</div>
 
+<h3 style="margin-left: 40px; color: white; padding-top: 5px; padding-bottom: 8px; border-top: 1px solid #a0a0a0; "> Albums <h3>
+<div class="gridViewContainer" style= "margin-left: 40px;">
+  
+    <?php
+    $albumQuery = mysqli_query($con, "SELECT * FROM albums  WHERE artist =".$artist_id." ORDER BY release_date desc");
+
+    while($row = mysqli_fetch_array($albumQuery)) {
+        
+        echo "<div class='gridViewItem' 
+        style=\"display: inline-block; margin-right: 20px; width: 29%; max-width: 200px; min-width: 150px; margin-bottom: 20px; \">
+        <span role='link' tabindex='0'  onclick='location.href=\"album_song_view.php?album_id=".$row['id']. "\"'>
+            <img src=\"assets/". $row['album_art_path'] ."\" style=\"width: 100%;\">
+            <div class='gridViewInfo' 
+            style=\"color: white; font-weight: 300; text-align: center; padding: 5px 0; text-overflow: ellipsis; overflow: hidden; white-space: nowrap;\">"
+                . $row['name'] .
+            "</div>
+        </span>
+    </div>";
+
+    }
+    echo "</div></div>";
+    
+    ?>
+</div>
 </body>
 </html>
